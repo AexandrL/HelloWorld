fd8ace4422b1f952302fdac40de0d332cf18ba85
AlexandrL <sasha_l@inbox.lv>
2022-11-06 19:42:23 +0200

    Init

--- src/com/digitalbear/javaguru/HelloWorld.java
+++ src/com/digitalbear/javaguru/HelloWorld.java
@@ -3,6 +3,8 @@ package com.digitalbear.javaguru;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("How are you doing?");
     }
 }
